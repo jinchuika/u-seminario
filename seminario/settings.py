b42b8cec8737c4eda588633a2b4a506e32160a94
Luis Carlos <jinchuika@gmail.com>
2016-10-04 18:34:39 -0600

    heroku

--- seminario/settings.py
+++ seminario/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'seminario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'seminario',
-        'USER': 'chuik',
-        'PASSWORD': 'passw2',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
